9172ed549e5a27e2f21934c5f05741b9e1b31d03
Dzmitry Solodov <solodow.mitya@yandex.by>
2022-05-21 21:41:14 +0300

    first-screen-adaptive

--- script/main.js
+++ script/main.js
@@ -1,6 +1,13 @@
 let buttonMenu = document.querySelector('.navigation__item__mobile-button');
+let navigation = document.querySelector('.navigation__list');
 
-buttonMenu.addEventListener('click', ()=>{
-    let navigation = document.querySelector('.navigation__list');
+document.addEventListener('click', event => {
+    let current =  navigation.classList.value;
+    if(event.target.classList.value !== "" && current.includes('visible') &&event.target.classList!==navigation.classList){
+        navigation.classList.remove('visible');
+    }
+})
+buttonMenu.addEventListener('click', ()=> {
     navigation.classList.toggle('visible');
 })
+
