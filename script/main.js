6e8a3c96ab2fc5d96f4069c0d83520a719e2c434
Dzmitry Solodov <solodow.mitya@yandex.by>
2022-05-23 16:45:46 +0300

    add webkit

--- script/main.js
+++ script/main.js
@@ -15,3 +15,11 @@ buttonMenu.addEventListener('click', ()=> {
 
 })
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
